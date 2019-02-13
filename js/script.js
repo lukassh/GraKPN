43bfea4f2b86b9c8f6ca0dd04ae1892e76d6f423
Lukass <lukassh@gmail.com>
2019-02-13 20:37:10 +0100

    Modify script.JS

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* // zmienne buttons
+ // zmienne buttons
 var uPapier = document.getElementById('btnPapier');
 var uKamien = document.getElementById('btnKamien');
 var uNozyczki = document.getElementById('btnNozyczki');
@@ -10,9 +10,21 @@ var rounds = document.getElementById('rounds');
 var infoRound = document.getElementById('infoRound');
 var matchResult = document.getElementById('matchResult');
 //zmienne globalne
-var player = { score: 0, round: 0, choice: ''  };
-var computer = { score: 0, round: 0, choice: ''};
-var rounds = { akt: 1, max: 0};
+
+
+var params = {
+  playerScore: 0,
+  playerRound: 0,
+  playerChoise: '';
+  computerScore: 0,
+  computerRound: 0,
+  computerChoise: '';
+  roundsWon: 0,
+  roundsLost: 0,
+  roundsAkt: 1;
+  roundsMax: 0;
+  gameDone: false;
+}
 
 
 // computer  move
@@ -23,51 +35,51 @@ var compMove = function(){
 
 // check winner
 var checkWinner = function(){
-   if(player.choise === computer.choise) { output.innerHTML = ('<strong>Remis!</strong>, You played ' +player.choise + ' and Computer played: ' +computer.choise); }
-  else if((player.choise === 'Kamień' && computer.choise === 'Nożyczki') || (player.choise === 'Papier' && computer.choise === 'Kamień') || (player.choise === 'Nożyczki' &&  computer.choise === 'Papier')
+   if(params.playerChoise === paramscomputerChoise) { output.innerHTML = ('<strong>Remis!</strong>, You played ' +params.playerChoise + ' and Computer played: ' +params.computerChoise); }
+  else if((params.playerChoise === 'Kamień' && params.computerChoise === 'Nożyczki') || (params.playerChoise === 'Papier' && params.computerChoise === 'Kamień') || (params.playerChoise === 'Nożyczki' &&  params.computerChoise === 'Papier')
   ){ 
-    output.innerHTML = ('You <strong>Won!</strong> You played: ' +player.choise + ' and Computer played: ' +computer.choise);
-    player.score++; 
+    output.innerHTML = ('You <strong>Won!</strong> You played: ' +params.playerChoise + ' and Computer played: ' +params.computerChoise);
+    params.playerScore++; 
    }
   else {
-    output.innerHTML = ('You <strong>Lose</strong>! You played: ' +player.choise + ' and Computer played: ' +computer.choise);
-    computer.score++;
+    output.innerHTML = ('You <strong>Lose</strong>! You played: ' +params.playerChoise + ' and Computer played: ' +params.computerChoise);
+    params.computerScore++;
   };
 };
 
 //adding text 
 var writeScore = function() {
-   results.innerHTML = ('Wynik rundy: ' +player.score + ' : ' +computer.score);
+   results.innerHTML = ('Wynik rundy: ' +params.playerScore + ' : ' +params.computerScore);
 };
 
 //check score - rounds
 var checkScore = function() {
-  if(player.score === 10) {
-    player.round++;
-    rounds.akt++;
-    player.score = 0; 
-    computer.score = 0;
+  if(params.playerScore === 10) {
+    params.playerRound++;
+    params.roundsAkt++;
+    params.playerScore = 0; 
+    params.computerScore = 0;
     output.innerHTML = ('Gratualcje wygrałeś rundę Runda: ');
   }  
-  else if(computer.score === 10){ 
-    computer.round++; 
-    rounds.akt++;
-    player.score = 0; 
-    computer.score = 0;
+  else if(params.computerScore === 10){ 
+   params.computerRound++; 
+    params.roundsAkt++;
+    params.playerScore = 0; 
+    params.computerScore = 0;
     output.innerHTML = ('Nietstye przegrałeś rundę Runda: ');
   }; 
-  rounds.innerHTML = ('Wynik meczu: ' +player.round + ' : ' +computer.round);
+  rounds.innerHTML = ('Wynik meczu: ' +params.playerRound + ' : ' params.computerRound);
 };
 
 // check match winner 
 var checkMatchWinner = function() {
-  if(player.round > computer.round) {
+  if(params.playerRound >params.computerRound) {
      matchResult.innerHTML = ('Gratulacje Wygrałeś !');
     }
-  else if(player.round === computer.round ) {
+  else if(params.playerRound ===params.computerRound ) {
     matchResult.innerHTML = ('Remis !');
     }
-  else if(player.round < computer.round) { 
+  else if(params.playerRound <params.computerRound) { 
   matchResult.innerHTML = ('Niestety przegrałeś !');
   };
 };
@@ -78,8 +90,9 @@ var disableBtn = function() {
 //start
 newGame.addEventListener('click', function() {
   
-  rounds.max = window.prompt('Ile rund gramy?: ');
-  infoRound.innerHTML = ('gramy ' +rounds.max + ' rundy.');
+  params.roundsMax = window.prompt('Ile rund gramy?: ');
+
+  infoRound.innerHTML = ('gramy ' +params.roundsMax + ' rundy.');
       uPapier.addEventListener("click", function(){ playerMove('Papier'); });
       uKamien.addEventListener('click', function(){ playerMove('Kamień'); });
       uNozyczki.addEventListener('click', function(){ playerMove('Nożyczki'); });
@@ -87,39 +100,25 @@ newGame.addEventListener('click', function() {
         var choise = ['Papier', 'Kamień', 'Nożyczki'];
           return choise[Math.floor(Math.random() *3)];
         }; 
+        var guziki = document.querySelectorAll('.player-move');
+          for ( var i=0; i<guziki.length; i++ ){
+            attr = this.getAttribute('.data-move');
+            playerMove(choise);
+          }
         var playerMove = function(choise) {
-            player.choise = choise;
-            computer.choise = compMove();
+            params.playerChoise = choise;
+            params.computerChoise = compMove();
             checkWinner();
             writeScore();
             checkScore(); 
-          infoRound.innerHTML = ('gramy ' +rounds.max + ' rundy. Runda: ' + rounds.akt + ' / ' +rounds.max);
-          if (rounds.akt > rounds.max) { 
+          infoRound.innerHTML = ('gramy ' +params.roundsMax + ' rundy. Runda: ' + params.roundsAkt + ' / ' +params.roundsMax);
+          if (params.roundsAkt > params.roundsMax) { 
             checkMatchWinner(); 
             disableBtn();
           };  
         };
  });
-*/
 
-var guziki = document.querySelectorAll('player-move');
-for ( var i=0; i<guziki.length; i++ ){
-  attr = this.getAttribute('data-move');
-  playerMove(attr);
-}
-
-var params = {
-  roundsPlayed: 0,
-  playerScore: 0,
-  computerScore: 0,
-  roundsWon: 0,
-  roundsLost: 0,
-  howManyRounds: 0;
-  gameDone: false;
-}
-var player = { score: 0, round: 0, choice: ''  };
-var computer = { score: 0, round: 0, choice: ''};
-var rounds = { akt: 1, max: 0};
 
   // Modale 
   
@@ -157,3 +156,4 @@ var rounds = { akt: 1, max: 0};
       event.stopPropagation();
     });
   }
+  // Koniec modali 
---
+++ script.js
@@ -0,0 +1,164 @@
+
+var newGame = document.getElementById('newGame');
+// zmienne tekst
+var output = document.getElementById('output');
+var results = document.getElementById('results');
+var rounds = document.getElementById('rounds');
+var infoRound = document.getElementById('infoRound');
+var matchResult = document.getElementById('matchResult');
+//zmienne globalne
+
+var params = {
+  playerScore: 0,
+  playerRound: 0,
+  playerChoise: '',
+  computerScore: 0,
+  computerRound: 0,
+  computerChoise: '',
+  roundsAkt: 1,
+  roundsMax: 1,
+}
+                                                // Funkcje **********************************
+    // computer  move
+    function compMove(){
+      var choise = ['Papier', 'Kamień', 'Nożyczki'];
+      return choise[Math.floor(Math.random() *3)];
+     };
+
+    // check winner
+    function checkWinner(){
+       if(params.playerChoise === params.computerChoise) { output.innerHTML = ('<strong>Remis!</strong><br>, You played ' +params.playerChoise + ' and Computer played: ' +params.computerChoise); }
+      else if((params.playerChoise === 'Kamień' && params.computerChoise === 'Nożyczki') || (params.playerChoise === 'Papier' && params.computerChoise === 'Kamień') || (params.playerChoise === 'Nożyczki' &&  params.computerChoise === 'Papier')
+      ){ 
+        output.innerHTML = ('You <strong>Won!</strong><br> You played: ' +params.playerChoise + ' and Computer played: ' +params.computerChoise);
+        params.playerScore++; 
+       }
+      else {
+        output.innerHTML = ('You <strong>Lose</strong>!<br> You played: ' +params.playerChoise + ' and Computer played: ' +params.computerChoise);
+        params.computerScore++;
+      };
+    };
+
+    //adding text 
+    function writeScore() {
+       results.innerHTML = ('Wynik rundy: ' +params.playerScore + ' : ' +params.computerScore);
+    };
+
+    //check score - rounds
+    function checkScore() {
+      if(params.playerScore === 10) {
+        params.playerRound++;
+        params.roundsAkt++;
+        params.playerScore = 0; 
+        params.computerScore = 0;
+        output.innerHTML = ('Gratualcje wygrałeś rundę Runda: ');
+      }  
+      else if(params.computerScore === 10){ 
+        params.computerRound++; 
+        params.roundsAkt++;
+        params.playerScore = 0; 
+        params.computerScore = 0;
+        output.innerHTML = ('Nietstye przegrałeś rundę Runda: ');
+      }; 
+      rounds.innerHTML = ('Wynik meczu: ' +params.playerRound + ' : ' +params.computerRound);
+    };
+
+    // check match winner 
+    function checkMatchWinner() {
+
+      if(params.playerRound > params.computerRound) {
+
+        openModal(modalResults);
+        matchResult.innerHTML = ('Gratulacje Wygrałeś !');
+
+        }
+      else if(params.playerRound === params.computerRound ) {
+        openModal(modalResults);
+        matchResult.innerHTML = ('Remis !');
+        }
+      else if(params.playerRound < params.computerRound) { 
+        openModal(modalResults);
+        matchResult.innerHTML = ('Niestety przegrałeś !');
+      };
+    };
+    function disableBtn() {
+      var buttons = document.getElementsByTagName('button');
+      for(i=0; i<5; i++) {  buttons[i].classList.toggle('dis'); };
+    };
+
+    function playerMove(choise) {
+                params.playerChoise = choise;
+                params.computerChoise = compMove();
+                checkWinner();
+                writeScore();
+                checkScore(); 
+              infoRound.innerHTML = ('gramy ' +params.roundsMax + ' rundy. Runda: ' + params.roundsAkt + ' / ' +params.roundsMax);
+              if (params.roundsAkt > params.roundsMax) { 
+                checkMatchWinner(); 
+                disableBtn();
+              };  
+            };
+
+    function compMove(){
+            var choise = ['Papier', 'Kamień', 'Nożyczki'];
+              return choise[Math.floor(Math.random() *3)];
+            }; 
+                                                           // START *********************************** 
+//start 
+newGame.addEventListener('click', function() {
+  
+  //uPapier.addEventListener("click", function(){ moves(); });
+  //uKamien.addEventListener('click', function(){ moves(); });
+  //uNozyczki.addEventListener('click', function(){ moves(); });
+  var buttonMoves = document.querySelectorAll('.player-move');   
+     
+  params.roundsMax = window.prompt('Ile rund gramy?: ');
+  infoRound.innerHTML = ('gramy ' +params.roundsMax + ' rundy.');
+  
+  for (var i=0; i < buttonMoves.length; i ++ ) {
+     
+     buttonMoves[i].addEventListener('click', function() {
+      var moveName = this.getAttribute('data-move');
+      playerMove(moveName);
+     });
+  } 
+ 
+  compMove();  
+});
+
+
+
+// modale                                                     MODALE ***********************************
+  
+  function openModal() {
+    document.getElementById('overlay').classList.add('show');
+    document.getElementById('modalResults').classList.add('show');
+  }; 
+  
+
+  function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+  };
+
+  document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault()
+      closeModal()
+    })
+  });
+
+  document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+      closeModal()
+    }
+  });
+
+  document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+      closeModal()
+    }
+  });
+
+
+
+
